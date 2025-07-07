832b1874606dd18d6ebbec2dcfd8a8d10d086962
Muhammed2tocka0 <shirinovafatime829@gmail.com>
2025-07-07 22:50:14 +0400

    Перенес index.html в корень репозитория

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http);
 
 const messages = []; // Хранилище сообщений
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 io.on('connection', (socket) => {
   let username = 'Гость';
